9b466f018f35eb7202d3c989688381a27e7f2b18
Urbaczka ivan <ivanneurbaczka@gmail.com>
2019-01-11 10:04:36 +0100

    charte graphique

--- js/main.js
+++ js/main.js
@@ -8,10 +8,12 @@ let choice =  0;
 function add(){
     choice++;
     return choice % 2 === 0 ? '0' : 'X';
+
 }
 
 
 cases.forEach(item => item.addEventListener('click',function(){
+    
     if(item.textContent)return;
     item.textContent = add();
 }))
