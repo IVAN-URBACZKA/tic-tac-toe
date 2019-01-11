33600ef28a7a957d03c105e1ae003d88d4d68492
Urbaczka ivan <ivanneurbaczka@gmail.com>
2019-01-11 09:33:51 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// récupérer les cases
+let cases = document.querySelectorAll('.case');
+cases = Array.from(cases);
+let choice =  0;
+
+
+
+function add(){
+    choice++;
+    return choice % 2 === 0 ? '0' : 'X';
+}
+
+
+cases.forEach(item => item.addEventListener('click',function(){
+    if(item.textContent)return;
+    item.textContent = add();
+}))
+   
+
